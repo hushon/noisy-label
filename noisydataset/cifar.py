d31b39810d9eeee58e5b036de0ba022e8f659a25
Hyounguk Shon <hyounguk.shon@kaist.ac.kr>
2023-04-17 05:39:37 +0000

    updated

--- noisydataset/cifar.py
+++ noisydataset/cifar.py
@@ -1,2 +1,11 @@
 import torch
 import torchvision
+
+
+class CIFAR10SyntheticNoise(torchvision.datasets.CIFAR10):
+    pass
+
+
+class CIFAR100SyntheticNoise(torchvision.datasets.CIFAR100):
+    pass
+
