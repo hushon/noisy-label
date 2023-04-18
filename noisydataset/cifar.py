2a0224ee1029a3ae87110c4d964d4b43d79475c3
Hyounguk Shon <hyounguk.shon@kaist.ac.kr>
2023-04-18 02:52:39 +0000

    implemented dataset

--- noisydataset/cifar.py
+++ noisydataset/cifar.py
@@ -70,6 +70,74 @@ class NoisyCIFAR10(torchvision.datasets.CIFAR10):
         }
 
 
+# class NoisyCIFAR10(torchvision.datasets.CIFAR10):
+#     """CIFAR-10 Dataset with synthetic label noise."""
+#     num_classes = 10
+
+#     def __init__(
+#             self,
+#             root: str,
+#             train: bool = True,
+#             transform = None,
+#             target_transform = None,
+#             download: bool = False,
+#             noise_rate: float = 0.2,
+#             noise_type: str = "symmetric",
+#             random_state: int = 42,
+#             ) -> None:
+#         super().__init__(root, train=train, transform=transform,
+#             target_transform=target_transform, download=download)
+#         assert self.train == True
+#         assert 0.0 <= noise_rate <= 1.0
+#         assert noise_type in ["symmetric", "asymmetric"]
+
+#         self.data = np.array(self.data)
+#         self.targets = np.array(self.targets)
+#         self.noise_rate = noise_rate
+#         self.random_state = random_state
+#         self.rng = np.random.default_rng(self.random_state)
+
+#         if noise_type == "symmetric":
+#             self.transition_matrix = self._symmetric_transition_matrix(self.num_classes, noise_rate)
+#         elif noise_type == "asymmetric":
+#             self.transition_matrix = self._asymmetric_transition_matrix(self.num_classes, noise_rate)
+#         self._inject_label_noise(self.transition_matrix)
+
+#     @staticmethod
+#     def _symmetric_transition_matrix(n, noise_rate):
+#         transition_matrix = np.full((n, n), noise_rate / (n - 1))
+#         np.fill_diagonal(transition_matrix, 1 - noise_rate)
+#         return transition_matrix
+
+#     @staticmethod
+#     def _asymmetric_transition_matrix(n, noise_rate):
+#         cifar10_asymm_label_transition = {
+#             9: 1,  # truck -> automobile
+#             2: 0,  # bird -> airplane
+#             3: 5,  # cat -> dog
+#             5: 3,  # dog -> cat
+#             4: 7,  # deer -> horse
+#         }
+#         transition_matrix = np.eye(n)
+#         for k, v in cifar10_asymm_label_transition.items():
+#             transition_matrix[k, k] = 1.0 - noise_rate
+#             transition_matrix[k, v] = noise_rate
+#         return transition_matrix
+
+#     def _inject_label_noise(self, transition_matrix):
+#         self.targets_gt = self.targets.copy()
+#         out_dist = transition_matrix[self.targets_gt]
+#         self.targets = torch.distributions.Categorical(torch.tensor(out_dist)).sample().numpy()
+
+#     def __getitem__(self, index):
+#         img, target = super().__getitem__(index)
+#         return {
+#             'image': img,
+#             'target': target,
+#             'target_gt': self.targets_gt[index],
+#         }
+
+
 class NoisyCIFAR100(torchvision.datasets.CIFAR100):
     """CIFAR-100 Dataset with synthetic label noise."""
     num_classes = 100
@@ -81,8 +149,8 @@ class NoisyCIFAR100(torchvision.datasets.CIFAR100):
             transform = None,
             target_transform = None,
             download: bool = False,
-            noise_rate : float = 0.2,
-            noise_type : str = "symmetric",
+            noise_rate: float = 0.2,
+            noise_type: str = "symmetric",
             random_state: int = 42,
             ) -> None:
         super().__init__(root, train=train, transform=transform,
@@ -98,30 +166,27 @@ class NoisyCIFAR100(torchvision.datasets.CIFAR100):
         self.rng = np.random.default_rng(self.random_state)
 
         if noise_type == "symmetric":
-            transition_matrix = self._symmetric_label_transition_matrix(self.num_classes, noise_rate)
+            self.transition_matrix = self._symmetric_transition_matrix(self.num_classes, noise_rate)
         elif noise_type == "asymmetric":
-            transition_matrix = self._asymmetric_label_transition_matrix(self.num_classes, noise_rate)
-        self._inject_label_noise(transition_matrix)
+            self.transition_matrix = self._asymmetric_transition_matrix(self.num_classes, noise_rate)
+        self._inject_label_noise(self.transition_matrix)
 
     @staticmethod
-    def _symmetric_label_transition_matrix(n, noise_rate):
+    def _symmetric_transition_matrix(n, noise_rate):
         transition_matrix = np.full((n, n), noise_rate / (n - 1))
         np.fill_diagonal(transition_matrix, 1 - noise_rate)
         return transition_matrix
 
     @staticmethod
-    def _asymmetric_label_transition_matrix(n, noise_rate):
+    def _asymmetric_transition_matrix(n, noise_rate):
         eye = np.eye(n, dtype=np.int32)
         transition_matrix = (1 - noise_rate) * eye + noise_rate * np.roll(eye, 1, axis=1)
         return transition_matrix
 
     def _inject_label_noise(self, transition_matrix):
         self.targets_gt = self.targets.copy()
-        eye = np.eye(self.num_classes, dtype=np.int32)
-        in_dist = eye[self.targets_gt] # one-hot encoding
-        out_dist = in_dist @ transition_matrix
-        self.targets = self.rng.choice(np.arange(len(out_dist)), p=out_dist)
-        pass
+        out_dist = transition_matrix[self.targets_gt]
+        self.targets = torch.distributions.Categorical(torch.tensor(out_dist)).sample().numpy()
 
     def __getitem__(self, index):
         img, target = super().__getitem__(index)
