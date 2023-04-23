b420c7b8687d7fc633a85bd9445c4707f1996645
Hyounguk Shon <hyounguk.shon@kaist.ac.kr>
2023-04-23 02:39:38 +0000

    added cifar100

---
+++ train_noisycifar100.sh
@@ -0,0 +1,11 @@
+python train.py --config configs/noisycifar100/train_ce_symm20.yml
+python train.py --config configs/noisycifar100/train_mae_symm20.yml
+
+python train.py --config configs/noisycifar100/train_ce_symm40.yml
+python train.py --config configs/noisycifar100/train_mae_symm40.yml
+
+python train.py --config configs/noisycifar100/train_ce_symm60.yml
+python train.py --config configs/noisycifar100/train_mae_symm60.yml
+
+python train.py --config configs/noisycifar100/train_ce_symm80.yml
+python train.py --config configs/noisycifar100/train_mae_symm80.yml
