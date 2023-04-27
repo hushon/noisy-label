48ee998b8336365d03d61621e80ff4305fe91443
Seunghee-Koh <seunghee1215@kaist.ac.kr>
2023-04-27 12:57:02 +0000

    Clothing1M dataset for our directory structure

--- datasets/clothing.py
+++ datasets/clothing.py
@@ -5,117 +5,92 @@ from typing import Tuple, Any
 from PIL import Image
 import os
 
+class Clothing1M(torchvision.datsets.ImageFolder):
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        transform = None,
+        target_transform = None
+        split: bool = ''
+        ) -> None: # No download, noise_type
+        """
+            Directory structure is different from Clothing1M official version..
+            [Our directory structure]
+            Clothing1M/
+            ├── clean_train
+            │ ├── 0
+            │ ├── ⋮
+            │ └── 13
+            ├── clean_test
+            │ ├── 0
+            │ ├── ⋮
+            │ └── 13
+            ├── clean_val
+            │ ├── 0
+            │ ├── ⋮
+            │ └── 13
+            └──noisy_train
+            ├── 0
+            ├── ⋮
+            └── 13
 
-class Clothing1M(torch.utils.data.Dataset): 
-    def __init__(self, root, transform, mode, num_samples=0, pred=[], probability=[], paths=[], num_class=14):
-        # Adapted from: https://github.com/LiJunnan1992/DivideMix/blob/master/dataloader_clothing1M.py
-        
-        self.root = root
-        self.transform = transform
-        self.mode = mode
-        self.train_labels = {}
-        self.test_labels = {}
-        self.val_labels = {}            
-        
-        with open('%s/noisy_label_kv.txt'%self.root,'r') as f:
-            lines = f.read().splitlines()
-            for l in lines:
-                entry = l.split()           
-                img_path = '%s/'%self.root+entry[0][7:]
-                self.train_labels[img_path] = int(entry[1])                         
-        with open('%s/clean_label_kv.txt'%self.root,'r') as f:
-            lines = f.read().splitlines()
-            for l in lines:
-                entry = l.split()           
-                img_path = '%s/'%self.root+entry[0][7:]
-                self.test_labels[img_path] = int(entry[1])   
+            [Args]
 
-        if mode == 'all':
-            train_imgs=[]
-            with open('%s/noisy_train_key_list.txt'%self.root,'r') as f:
-                lines = f.read().splitlines()
-                for l in lines:
-                    img_path = '%s/'%self.root+l[7:]
-                    train_imgs.append(img_path)                                
-            random.shuffle(train_imgs)
-            class_num = torch.zeros(num_class)
-            self.train_imgs = []
-            for impath in train_imgs:
-                label = self.train_labels[impath] 
-                if class_num[label]<(num_samples/14) and len(self.train_imgs)<num_samples:
-                    self.train_imgs.append(impath)
-                    class_num[label]+=1
-            random.shuffle(self.train_imgs)       
-        elif self.mode == "labeled":   
-            train_imgs = paths 
-            pred_idx = pred.nonzero()[0]
-            self.train_imgs = [train_imgs[i] for i in pred_idx]                
-            self.probability = [probability[i] for i in pred_idx]            
-            print("%s data has a size of %d"%(self.mode,len(self.train_imgs)))
-        elif self.mode == "unlabeled":  
-            train_imgs = paths 
-            pred_idx = (1-pred).nonzero()[0]  
-            self.train_imgs = [train_imgs[i] for i in pred_idx]                
-            self.probability = [probability[i] for i in pred_idx]            
-            print("%s data has a size of %d"%(self.mode,len(self.train_imgs)))                                    
-                         
-        elif mode=='test':
-            self.test_imgs = []
-            with open('%s/clean_test_key_list.txt'%self.root,'r') as f:
-                lines = f.read().splitlines()
-                for l in lines:
-                    img_path = '%s/'%self.root+l[7:]
-                    self.test_imgs.append(img_path)            
-        elif mode=='val':
-            self.val_imgs = []
-            with open('%s/clean_val_key_list.txt'%self.root,'r') as f:
-                lines = f.read().splitlines()
-                for l in lines:
-                    img_path = '%s/'%self.root+l[7:]
-                    self.val_imgs.append(img_path)
-                    
-    def __getitem__(self, index):  
-        if self.mode=='labeled':
-            img_path = self.train_imgs[index]
-            target = self.train_labels[img_path] 
-            prob = self.probability[index]
-            image = Image.open(img_path).convert('RGB')    
-            img1 = self.transform(image) 
-            img2 = self.transform(image) 
-            return img1, img2, target, prob              
-        elif self.mode=='unlabeled':
-            img_path = self.train_imgs[index]
-            image = Image.open(img_path).convert('RGB')    
-            img1 = self.transform(image) 
-            img2 = self.transform(image) 
-            return img1, img2  
-        elif self.mode=='all':
-            img_path = self.train_imgs[index]
-            target = self.train_labels[img_path]     
-            image = Image.open(img_path).convert('RGB')   
-            img = self.transform(image)
-            return img, target, img_path        
-        elif self.mode=='test':
-            img_path = self.test_imgs[index]
-            target = self.test_labels[img_path]     
-            image = Image.open(img_path).convert('RGB')   
-            img = self.transform(image) 
-            return img, target
-        elif self.mode=='val':
-            img_path = self.val_imgs[index]
-            target = self.test_labels[img_path]     
-            image = Image.open(img_path).convert('RGB')   
-            img = self.transform(image) 
-            return img, target    
-        
-    def __len__(self):
-        if self.mode=='test':
-            return len(self.test_imgs)
-        if self.mode=='val':
-            return len(self.val_imgs)
+            train   bool        True    : getitem from train set
+                                False   : getitem from test / val set
+
+            transform           pytorch transform
+
+            target_transform    pytorch target transform
+
+            split   str         clean       : clean_train if train is True,  else NotImplementedError
+                                ''(default) : noisy_train if train is True,  else clean_test
+                                val         : clean_val   if train is False, else NotImplementedError
+        """
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        transform = None,
+        target_transform = None
+        split: bool = ''
+        ) -> None: # No download, noise_type
+        # Adapted from https://github.com/Yikai-Wang/SPR-LNL/blob/af29e431bf7bd4d7840ce87c08b2ae9ebafeb7d1/dataset.py#L331
+        # This implementation does not support cutmix mode / label updating.
+        # Animal-10N ftp 들어가보면 raw_img ver과 binary ver 있는데, raw_img ver이 맞는것같다.(label을 path.split으로 정하는것을 보니)
+        self.train = train
+        if self.train:
+            if split in ['', 'noisy']:
+                split = 'noisy_train'
+            elif split == 'clean':
+                split = 'clean_train'
+            else:
+                raise NotImplementedError
         else:
-            return len(self.train_imgs)            
+            if split in ['', 'test']:
+                split = 'clean_test'
+            elif split == 'val':
+                split = 'clean_val'
+            else:
+                raise NotImplementedError
+
+        root = os.path.join(root, split)
+        super().__init__(root=root,
+                         transform=transform,
+                         target_transform=target_transform
+                         )
+
+    def __getitem__(self, index):
+        image, target = super().__getitem__(index)
+        return {
+            'image': image,
+            'target': target
+        }# No target_gt: real-world noisy dataset.
 
 
 if __name__ == '__main__':
-    pass
+    import pdb
+    root = './datasets/Clothing1M'
+    animal_dset = Animal10(root, train=True)
+    pdb.set_trace()
