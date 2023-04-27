3ccd2c6fcf801df6ca5bf83f34d7e4b9e887e2d3
Hyounguk Shon <hyounguk.shon@kaist.ac.kr>
2023-04-27 17:42:43 +0000

    cleaned

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from torch import nn
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
-from .module import MCDropout, GaussianDropout, GaussianMCDropout
+from .module import MCDropout, GaussianDropout, GaussianMCDropout, MeanAbsoluteError
 
 
 def get_model(architecture, num_classes) -> nn.Module:
--- models/module.py
+++ models/module.py
@@ -3,6 +3,31 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class MeanAbsoluteError(torch.nn.Module):
+    def __init__(self, reduction="mean"):
+        super().__init__()
+        self.reduction = reduction
+
+    def forward(self, pred, labels):
+        pred = F.softmax(pred, dim=1)
+        mae = 1. - torch.gather(pred, 1, labels.view(-1, 1))
+        mae = mae.squeeze(1)
+        # Note: Reduced MAE
+        # Original: torch.abs(pred - label_one_hot).sum(dim=1)
+        # $MAE = \sum_{k=1}^{K} |\bm{p}(k|\bm{x}) - \bm{q}(k|\bm{x})|$
+        # $MAE = \sum_{k=1}^{K}\bm{p}(k|\bm{x}) - p(y|\bm{x}) + (1 - p(y|\bm{x}))$
+        # $MAE = 2 - 2p(y|\bm{x})$
+        #
+        if self.reduction == "mean":
+            return mae.mean()
+        elif self.reduction == "sum":
+            return mae.sum()
+        elif self.reduction == "none":
+            return mae
+        else:
+            raise NotImplementedError
+
+
 class MCDropout(nn.Dropout):
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return F.dropout(input, self.p, True, self.inplace)
@@ -23,12 +48,7 @@ class GaussianDropout(nn.Module):
             return x
 
 
-class GaussianMCDropout(nn.Module):
-    def __init__(self, p=0.5):
-        super().__init__()
-        assert 0.0 <= p <= 1.0
-        self.p = p
-
+class GaussianMCDropout(GaussianDropout):
     def forward(self, x):
         stddev = (self.p / (1.0 - self.p))**0.5
         epsilon = torch.randn_like(x) * stddev
