abcd7a62c3763d6246d98b8c2a9cab1121389520
Seunghee-Koh <seunghee1215@kaist.ac.kr>
2023-04-27 06:13:28 +0000

    Animal 10N dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,3 @@
 from .cifar import CIFAR10, CIFAR100, NoisyCIFAR10, NoisyCIFAR100, CIFAR10N, CIFAR100N
+from .food101 import Food101
+from animal import Animal10N
--- datasets/animal.py
+++ datasets/animal.py
@@ -2,54 +2,60 @@ import torch
 import torchvision
 import numpy as np
 from typing import Tuple, Any
-from torchvision
+from PIL import Image
+import os
 
-     
-        
-class Animal10(Dataset):
 
-    def __init__(self, split='train', data_path=None, transform=None, cutmix=False):
-        self.train = split == 'train'
-        self.cutmix = cutmix
+class Animal10N(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        root: str,
+        train: bool = True,
+        transform = None,
+        target_transform = None
+        ) -> None: # No download, noise_type
+        # Adapted from https://github.com/Yikai-Wang/SPR-LNL/blob/af29e431bf7bd4d7840ce87c08b2ae9ebafeb7d1/dataset.py#L331
+        # This implementation does not support cutmix mode / label updating.
+        # Animal-10N ftp 들어가보면 raw_img ver과 binary ver 있는데, raw_img ver이 맞는것같다.(label을 path.split으로 정하는것을 보니)
+        self.train = train
+        split = 'train' if self.train else 'test'
 
-        data_path = data_path.lower()
-
-        self.image_dir = os.path.join(data_path, split + 'ing')
-
-        self.image_files = [f for f in listdir(self.image_dir) if os.path.isfile(os.path.join(self.image_dir, f))]
+        self.root = root
+        self.image_dir = os.path.join(self.root, f'{split}ing')
+        self.image_files = [f for f in os.listdir(self.image_dir) if os.path.isfile(os.path.join(self.image_dir, f))]
 
         self.targets = []
-
         for path in self.image_files:
             label = path.split('_')[0]
             self.targets.append(int(label))
 
-        self.mislabeled_targets = self.targets
-
         self.transform = transform
-
-    def __getitem__(self, index):
-        image_path = os.path.join(self.image_dir, self.image_files[index])
-
-        image_origin = Image.open(image_path)
-
-        if self.transform is not None:
-            image = self.transform(image_origin)
-            if self.cutmix:
-                image1 = self.transform(image_origin)
-
-        label = self.targets[index]
-        label = torch.from_numpy(np.array(label).astype(np.int64))
-
-        if self.train:
-            if self.cutmix:
-                return image, image1, label, index
-            return image, label, index
-        else:
-            return image, label
+        self.target_transform = target_transform
 
     def __len__(self):
         return len(self.targets)
 
-    def update_corrupted_label(self, noise_label):
-        self.targets[:] = noise_label[:]
+    def __getitem__(self, index):
+        image_path = os.path.join(self.image_dir, self.image_files[index])
+        image = Image.open(image_path)
+
+        # target = self.targets[index]
+        # target = torch.from_numpy(np.array(target).astype(np.int64)) # why?
+        target = torch.tensor(self.targets[index]) # It may be same as commented above. (shape, dtype)
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return {
+            'image': image,
+            'target': target
+        } # No target_gt: real-world noisy dataset.
+
+if __name__ == '__main__':
+    import pdb
+    root = './data/Animal-10N'
+    animal_dset = Animal10(root, train=True)
+    pdb.set_trace()
