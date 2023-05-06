132ca4d4f67212e7e52c83ae5cd22b51a509142d
Hyounguk Shon <hyounguk.shon@kaist.ac.kr>
2023-05-06 15:45:53 +0000

    updated data configs

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -20,14 +20,14 @@ def get_dataset(**kwargs) -> Tuple[data.Dataset, data.Dataset]:
     data_root = "./data"
     dataset = kwargs.pop('dataset')
     if dataset == "noisy_cifar10":
-        train = NoisyCIFAR10(data_root, download=True, transform=transform_train, **kwargs)
-        test = CIFAR10(data_root, download=True, train=False, transform=transform_test)
+        train = NoisyCIFAR10(data_root, transform=transform_train, **kwargs)
+        test = CIFAR10(data_root, train=False, transform=transform_test)
     elif dataset == "noisy_cifar100":
-        train = NoisyCIFAR100(data_root, download=True, transform=transform_train, **kwargs)
-        test = CIFAR100(data_root, download=True, train=False, transform=transform_test)
+        train = NoisyCIFAR100(data_root, transform=transform_train, **kwargs)
+        test = CIFAR100(data_root, train=False, transform=transform_test)
     elif dataset == "noisy_cifar3":
-        train = NoisyCIFAR3(data_root, download=True, train=True, transform=transform_train, **kwargs)
-        test = NoisyCIFAR3(data_root, download=True, train=False, transform=transform_test, **kwargs)
+        train = NoisyCIFAR3(data_root, train=True, transform=transform_train, **kwargs)
+        test = NoisyCIFAR3(data_root, train=False, transform=transform_test, **kwargs)
     else:
         raise NotImplementedError
     return train, test
