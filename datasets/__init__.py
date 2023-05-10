0efd07accc7e8259e6e3f39f5c693c7c7e5d1669
Hyounguk Shon <hyounguk.shon@kaist.ac.kr>
2023-05-10 05:29:33 +0000

    cleaned get_data

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,32 +3,55 @@ from .food101 import Food101
 from .animal import Animal10N
 from torchvision import transforms
 from typing import Tuple
-import torch.utils.data as data
+from torch.utils.data import Dataset
 
 
-def get_dataset(**kwargs) -> Tuple[data.Dataset, data.Dataset]:
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        # transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
+def get_dataset(**kwargs) -> Tuple[Dataset, Dataset]:
     data_root = "./data"
-    dataset = kwargs.pop('dataset')
-    if dataset == "noisy_cifar10":
-        train = NoisyCIFAR10(data_root, transform=transform_train, **kwargs)
-        test = CIFAR10(data_root, train=False, transform=transform_test)
-    elif dataset == "noisy_cifar100":
-        train = NoisyCIFAR100(data_root, transform=transform_train, **kwargs)
-        test = CIFAR100(data_root, train=False, transform=transform_test)
-    elif dataset == "noisy_cifar3":
-        train = NoisyCIFAR3(data_root, train=True, transform=transform_train, **kwargs)
-        test = NoisyCIFAR3(data_root, train=False, transform=transform_test, **kwargs)
-    else:
-        raise NotImplementedError
-    return train, test
+    dataset_name = kwargs.pop('dataset')
+
+    match dataset_name:
+        case "noisy_cifar10":
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+            transform_test = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+            train_dataset = NoisyCIFAR10(data_root, transform=transform_train, **kwargs)
+            test_dataset = CIFAR10(data_root, train=False, transform=transform_test)
+
+        case "noisy_cifar100":
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+            transform_test = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+            train_dataset = NoisyCIFAR100(data_root, transform=transform_train, **kwargs)
+            test_dataset = CIFAR100(data_root, train=False, transform=transform_test)
+        case "noisy_cifar3":
+            transform_train = transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+            transform_test = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+            ])
+            train_dataset = NoisyCIFAR3(data_root, train=True, transform=transform_train, **kwargs)
+            test_dataset = NoisyCIFAR3(data_root, train=False, transform=transform_test, **kwargs)
+        case _:
+            raise NotImplementedError
+
+    return train_dataset, test_dataset
